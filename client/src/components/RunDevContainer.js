aac6736130ba429fe86c13163e194e374c737680
Haseebz1 <haseebkandore@hotmail.com>
2023-09-10 00:24:53 +0100

    RunDev Container created, passing current Page from state and function to update it, also render method added

--- client/src/components/RunDevContainer.js
+++ client/src/components/RunDevContainer.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import NavTabs from './NavTabs';
 import Footer from './Footer';
-import Header from './Header';
-import HomePage from './Pages/Homepage';
+// import Header from './Header';
+import MyProfile from './Pages/MyProfile';
 import LogIn from './Pages/LogIn';
-import Profile from './Pages/Profile';
+import Scripts from './Pages/Scripts'
 
+export default function RunDevContainer() {
+    const [currentPage, setCurrentPage] = useState('MyProfile');
+
+    const renderPage = () => {
+        if (currentPage === 'MyProfile') {
+            return <MyProfile />;
+        }
+        if (currentPage === 'logIn' ) {
+            return <LogIn />;
+        }
+        if (currentPage === 'Scripts') {
+            return <Scripts />;
+        }
+    }
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+            <Footer />
+        </div>
+    )
+}
