e6f6aa135aa6990c33c5478b39e6e9a27eaf62e9
Haseebz1 <haseebkandore@hotmail.com>
2023-09-10 21:33:06 +0100

    basic header code added

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Header.css';
+
+function Header() {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+                <span>RunDev</span>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- client/src/components/RunDevContainer.js
+++ client/src/components/RunDevContainer.js
@@ -1,34 +1,39 @@
 import React, { useState } from "react";
-import NavTabs from './NavTabs';
-import Footer from './Footer/Index';
-// import Header from './Header';
-import MyProfile from './Pages/MyProfile';
-import LogIn from './Pages/LogIn';
+import NavTabs from "./NavTabs";
+import Footer from "./Footer/Index";
+import Header from "./Header/index";
+import MyProfile from "./Pages/MyProfile";
+import LogIn from "./Pages/LogIn";
 
-import Scripts from './Pages/Scripts'
+import Scripts from "./Pages/Scripts";
 
 export default function RunDevContainer() {
-    const [currentPage, setCurrentPage] = useState('MyProfile');
+  const [currentPage, setCurrentPage] = useState("MyProfile");
 
-    const renderPage = () => {
-        if (currentPage === 'MyProfile') {
-            return <MyProfile />;
-        }
-        if (currentPage === 'logIn' ) {
-            return <LogIn />;
-        }
-        if (currentPage === 'Scripts') {
-            return <Scripts />;
-        }
+  const renderPage = () => {
+    if (currentPage === "MyProfile") {
+      return <MyProfile />;
     }
+    if (currentPage === "logIn") {
+      return <LogIn />;
+    }
+    if (currentPage === "Scripts") {
+      return <Scripts />;
+    }
+  };
 
-    const handlePageChange = (page) => setCurrentPage(page);
+  const handlePageChange = (page) => setCurrentPage(page);
 
-    return (
-        <div>
-            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-            {renderPage()}
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <div Header>
+        <NavTabs
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}
+        />
+        {renderPage()}
+        <Footer />
+      </div>
+    </div>
+  );
 }
