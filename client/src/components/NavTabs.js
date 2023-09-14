58b94080fd120e3b0296e934ca188a2054416cbb
AMillsy <00adammills00@gmail.com>
2023-09-14 14:12:33 +0100

    setting up logout functionality

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -1,28 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navtabs.css";
 import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
-import { QUERY_ME } from "../utils/query";
+import { QUERY_ME_USERNAME } from "../utils/query";
 import { useQuery } from "@apollo/client";
-function NavTabs() {
-  function isLoggedIn() {
-    const loggedIn = Auth.loggedIn();
 
-    if (loggedIn) {
-      return <Link to={"/me"}>Username</Link>;
+function NavTabs() {
+  const [username, setUsername] = useState("");
+
+  const loggedIn = Auth.loggedIn();
+  const { loading, data, error } = useQuery(QUERY_ME_USERNAME, {
+    skip: !loggedIn || username !== "",
+  });
+
+  function isLoggedIn() {
+    if (loading) return;
+    if (username) {
+      return (
+        <Link className="nav-item" to={"/me"}>
+          {username}
+        </Link>
+      );
+    } else if (data) {
+      setUsername(data?.me.username);
+      return (
+        <Link className="nav-item" to={"/me"}>
+          {username}
+        </Link>
+      );
     }
-    return <Link to={"/login"}>Login</Link>;
+    return (
+      <Link className="nav-item" to={"/login"}>
+        Login
+      </Link>
+    );
+  }
+  function logout() {
+    //Log the user out and return them to the homepage
+  }
+  function addLogout() {
+    if (loggedIn)
+      return (
+        <li>
+          <Link onClick={() => logout()} className="logout-btn nav-item">
+            Logout
+          </Link>
+        </li>
+      );
   }
   return (
     <nav>
       <ul>
         <li>
-          <Link to={"/"}>Scripts</Link>
-        </li>
-        <li>
-          <Link to={"/profile"}>Profile</Link>
+          <Link className="nav-item" to={"/"}>
+            Scripts
+          </Link>
         </li>
         <li>{isLoggedIn()}</li>
+        {addLogout()}
       </ul>
     </nav>
   );
