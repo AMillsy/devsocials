fae13593c920e273e72ca20e3971849c565b56fd
Haseebz1 <haseebkandore@hotmail.com>
2023-09-14 19:12:46 +0100

    css styling added in comment popup

--- client/src/components/CommentPopUp.js
+++ client/src/components/CommentPopUp.js
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_COMMENTS_QUERY } from "../utils/query";
 import { CREATE_COMMENT } from "../utils/mutations";
+import './CommentPopUp.css';
 
 const CommentPopup = ({ isOpen, onRequestClose, postId }) => {
   const { data, loading, error } = useQuery(GET_COMMENTS_QUERY, {
@@ -34,7 +35,7 @@ const CommentPopup = ({ isOpen, onRequestClose, postId }) => {
         },
       });
       setCommentText("");
-      refetch();
+      // refetch();
     } catch (err) {
       console.error("Error adding comment", err);
     }
@@ -45,12 +46,13 @@ const CommentPopup = ({ isOpen, onRequestClose, postId }) => {
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel="Comment Popup"
+      className="comment-popup"
     >
-      <h2>Comments</h2>
-      <ul>
+      <h2 className="comment-popup-title">Comments</h2>
+      <ul className="comment-list">
         {comments.map((comment) => (
-          <li style={{ color: "black" }} key={comment._id}>
-            {comment.message}
+          <li className="comment-item" style={{ color: "black" }} key={comment._id}>
+           <span className="comment-text" >{comment.message}</span>
           </li>
         ))}
       </ul>
@@ -59,10 +61,11 @@ const CommentPopup = ({ isOpen, onRequestClose, postId }) => {
           value={commentText}
           onChange={(e) => setCommentText(e.target.value)}
           placeholder="Write a comment..."
+          className="comment-input"
         />
-        <button onClick={handleCommentSubmit}>Add Comment</button>
+        <button onClick={handleCommentSubmit} className="add-comment-button">Add Comment</button>
       </div>
-      <button onClick={onRequestClose}>Close</button>
+      <button onClick={onRequestClose} className="close-button">Close</button>
     </Modal>
   );
 };
